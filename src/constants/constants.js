01a220eaed58f51531dc630a2c7145e305aaaf5d
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-02-23 13:46:39 -0300

    adds spotify project to portifolio

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,13 +1,32 @@
 export const projects = [
   {
+    title: 'Discogs to Spotify',
+    description: "[Projeto Pessoal] Discogs to Spotify é uma aplicação que permite que usuários do marketplace de discos Discogs consigam importar suas coleções para playlists no Spotify. Tem usabilidade simples e intuitiva e seu principal propósito é integrar a API do Discogs com a API do Spotify. Baster fazer login com sua conta do Spotify e começar a importar! (Você pode testar a aplicação mesmo se não for um usuário do Discogs). A ideia do projeto surgiu ao me deparar com a grande quantidade de pessoas nos foruns do Discogs solicitando alguma aplicação que pudesse fazer essa importação de forma automatizada.",
+      image: '/images/spotify.png',
+      tags: ['React', 'Next', 'next-auth', 'Sass'],
+    source: 'https://jsnotes-client2.herokuapp.com/',
+    visit: 'https://github.com/gregisb/Discogs-to-Spotify',
+    id: 0,
+  },
+  {
+    title: 'Pomodoro Tasks',
+    description: "Pomodoro Tasks é uma aplicação que une duas funcionalidades para produtividade: Pomodoro Timer e Todo List. O usuário pode adcionar tarefas na lista fazer uso da técnica pomodoro, com as opção de 25 minutos para trabalho, 5 minutos para short break e 15 minutos para longbreak. As tarefas ficam armazenadas no localStorage.",
+    image: '/images/pomodorotasks.png',
+    tags: ['React', 'JavaScript', 'CSS'],
+    source: 'https://pomodoro-tasks-psi.vercel.app/',
+    visit: 'https://github.com/gregisb/Pomodoro-Tasks',
+    id: 2,
+  },
+  {
     title: 'JavaScript Notes',
     description: "JavaScript Notes é uma aplicação completa baseada em aplicações como Evernote. Nesse projeto o usuário pode fazer um cadastro e ter acesso a uma plataforma em que pode adcionar, salvar e alterar notas. Também é possível que o usuário modifique suas credenciais, como e-mail, senha e nome. Aplicação completa (front-end e back-end) e responsiva. (Hospedado no Heroku)",
       image: '/images/jsnotes.png',
       tags: ['React', 'Node', 'Express', 'MongoDB'],
     source: 'https://jsnotes-client2.herokuapp.com/',
     visit: 'https://github.com/gregisb/JavaScript-notes-Client',
-    id: 0,
+    id: 1,
   },
+  
   {
     title: 'Podcastr',
     description:"Podcastr é um projeto desenvolvido para reprodução de podcasts. Todo o front-end foi desenvolvido do zero, com os componentes e todas funcionalidades do player, como play, pause, previous, next, loop e repeat! No projeto estão sendo exibidos a lista de episódios da RocketSeat, através da API.",
@@ -15,7 +34,7 @@ export const projects = [
     tags: ['React', 'Next', 'SASS', 'styled-components'],
     source: 'https://gr-podcastr.vercel.app/',
     visit: 'https://github.com/gregisb/podcastrNLW',
-    id: 1,
+    id: 4,
   },
   {
     title: 'Headphones - Website',
@@ -24,18 +43,8 @@ export const projects = [
       tags: ['JavaScript', 'SASS', 'CSS', 'HTML'],
     source: 'https://gr-headphones-webpage.vercel.app/',
     visit: 'https://github.com/gregisb/Headphones-webpage',
-    id: 2,
-  },
-  {
-    title: 'Pomodoro Tasks',
-    description: "Pomodoro Tasks é uma aplicação que une duas funcionalidades para produtividade: Pomodoro Timer e Todo List. O usuário pode adcionar tarefas na lista fazer uso da técnica pomodoro, com as opção de 25 minutos para trabalho, 5 minutos para short break e 15 minutos para longbreak. As tarefas ficam armazenadas no localStorage.",
-    image: '/images/pomodorotasks.png',
-    tags: ['React', 'React Hooks', 'JavaScript', 'CSS'],
-    source: 'https://pomodoro-tasks-psi.vercel.app/',
-    visit: 'https://github.com/gregisb/Pomodoro-Tasks',
     id: 3,
   },
-  
 ];
 
 export const TimeLineData = [
